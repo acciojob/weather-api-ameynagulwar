d0cd38f7421ed023a73f95b6778131a5daedd7ab
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-06-24 14:11:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+describe("Weather API", () => {
+  it("displays current weather for London", () => {
+    // Visit the application URL
+    cy.visit("http://your-app-url.com");
+
+    // Wait for the weather button to become visible
+    cy.get("#weatherButton").should("be.visible");
+
+    // Intercept the API request and provide a response
+    cy.intercept("GET", "https://api.openweathermap.org/data/2.5/weather**").as(
+      "getCurrentWeather"
+    );
+
+    // Click the weather button
+    cy.get("#weatherButton").click();
+
+    // Wait for the API request to be made and get the response
+    cy.wait("@getCurrentWeather").then((interception) => {
+      // Extract the weather data from the response
+      const weatherData = interception.response.body;
+
+      // Assert on the weather data or perform other assertions as needed
+      expect(weatherData.name).to.equal("London");
+      expect(weatherData.weather[0].description).to.be.a("string");
+
+      // Assert that the weather data is displayed in the div element
+      cy.get("#weatherData").should(
+        "have.text",
+        `Current weather in London: ${weatherData.weather[0].description}`
+      );
+    });
+  });
+});

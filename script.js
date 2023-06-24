a36c0e578c24f52514a8d7f093585be3d16954ab
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-06-24 14:19:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 function getCurrentWeather() {
-    // Make a GET request to the weather API
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY')
+    return fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY')
         .then(response => response.json())
-        .then(data => {
-            // Extract the weather description from the API response
-            const weatherDescription = data.weather[0].description;
-
-            // Update the weatherData div with the weather information
-            const weatherDataElement = document.getElementById('weatherData');
-            weatherDataElement.textContent = `Current weather in London: ${weatherDescription}`;
-        })
+        .then(data => data.weather[0].description)
         .catch(error => {
             console.error('Error:', error);
+            throw error;
         });
 }
+
+// Export the function for Cypress test
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = {
+        getCurrentWeather
+    };
+}
+
+describe('Weather Application', () => {
+    it('displays current weather for London', () => {
+        cy.visit('index.html');
+
+        // Stub the API request
+        cy.server();
+        cy.route('GET', 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY', {
+            weather: [{
+                description: 'Clouds'
+            }]
+        }).as('getCurrentWeather');
+
+        // Trigger the button click and wait for the API response
+        cy.get('button').click();
+        cy.wait('@getCurrentWeather').its('status').should('eq', 200);
+
+        // Verify the weather data in the DOM
+        cy.get('#weatherData').should('have.text', 'Current weather in London: Clouds');
+    });
+});
